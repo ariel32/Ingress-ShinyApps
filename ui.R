11031df88d03c9850ff076292528a6142104a409
ariel32 <argentum32@gmail.com>
2014-11-08 20:09:53 +0300

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(gstat)
+library(sp)
+library(raster)
+library(rasterVis)
+library(colorRamps)
+
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      selectInput("agent", label = h3("Select box"), 
+                  list("capsula" = 1, "KORN" = 2, "joras" = 3, "MalenkiyKiller" = 4), 
+                  selected = 1)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+)
